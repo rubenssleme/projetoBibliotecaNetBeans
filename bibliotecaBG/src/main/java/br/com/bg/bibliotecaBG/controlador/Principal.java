c03768c39f531d6e30f93370e5b19bb96f1a50b9
rubens leme <rubenss.leme@gmail.com>
2018-09-20 20:36:59 -0300

    Criação dos Pacotes e Preparação da classe Principal.

---
+++ bibliotecaBG/src/main/java/br/com/bg/bibliotecaBG/controlador/Principal.java
@@ -0,0 +1,16 @@
+
+package br.com.bg.bibliotecaBG.controlador;
+
+import br.com.bg.bibliotecaBG.tela.TelaMenu;
+
+/**
+ *
+ * @author rubens leme
+ * @since 20/09/2018
+ */
+public class Principal {
+    public static void main(String[] args) {
+        TelaMenu telaMenu = new TelaMenu();
+        telaMenu.setVisible(true);
+    }
+}
